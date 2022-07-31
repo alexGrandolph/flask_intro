06becc5cce623de130e0e63b0444c4e8072d4cde
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-31 11:29:49 -0600

    feat: add logic to update a task, update page now functioning

--- app.py
+++ app.py
@@ -47,11 +47,20 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
+  task = Todo.query.get_or_404(id)
+
   if request.method == 'POST':
-    pass
+    task.content = request.form['content']
+
+    try:
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was a problem updating the task' 
+   
 
   else:
-    return render_template('update.html')
+    return render_template('update.html', task=task)
 
 
 if __name__ == '__main__':
